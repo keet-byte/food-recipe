7ad976ea3b984651a1c1ad2d3c8e1f11546e6a77
Keerthana9823 <keerthana.pazhanivel@accenture.com>
2024-09-08 20:46:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Routes , Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Favorites from './pages/favorites';
+import Details from './pages/Details';
+import Navbar from './Components/navbar';
+
+
+function App() {
+  return (
+    <div>
+    <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>
+    <Navbar />
+    <Routes>
+      <Route path='/' element ={ <Home/>}/>
+      <Route path='/favorites' element ={ <Favorites/> }/>
+      <Route path='/recipe-item/:id' element= { <Details/> }/>
+    </Routes>
+   </div>
+   </div>
+  )
+}
+
+export default App
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
+import RecipeItem from "../../Components/recipe-item";
+
+export default function Home() {
+  const { loading, recipeList } = useContext(GlobalContext);
+
+  if (loading) return <div>Loading...Please wait!</div>;
+
+  return (
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => {
+          return <RecipeItem item={item} key={item.id} />;
+        })
+      ) : (
+        <div>
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">Try some other recipe..</p>
+        </div>
+      )}
+    </div>
+  );
+}
